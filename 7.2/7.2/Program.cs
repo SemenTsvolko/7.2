e52da903407cb5eecfc60a53f21a3b03b9a05f90
SemenTsvolko <75906654+SemenTsvolko@users.noreply.github.com>
2020-12-25 20:06:02 +0300

    Add files via upload

---
+++ 7.2/7.2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _7._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите <№> и любое число :");
+            string str = Console.ReadLine();
+            
+            while (str.Contains("№"))
+            {
+                str = str.Replace("№", "номер по порядку ");
+            }
+            Console.WriteLine(str);
+            Console.ReadKey();
+        }
+    }
+}
